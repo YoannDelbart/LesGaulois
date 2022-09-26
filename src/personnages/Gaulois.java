05179e3bd1a0a5feeb20a9041bc45a7cf373f16c
delbart yoann <dln4421a@ens.fsi>
2022-09-26 20:00:43 +0200

    2aP17

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -9,9 +9,25 @@ public class Gaulois {
 		this.nom = nom;
 		this.force = force;
 	}
-
+	
 	public String getNom() {
 		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + " << " + texte + " >> ");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois" + nom + " : ";
+	}
+	
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+		
+		
 	}
 
 	@Override
@@ -23,6 +39,9 @@ public class Gaulois {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		System.out.println(asterix.getNom()); 
 		System.out.println(asterix);
+		asterix.prendreParole();
+		
+		
 		 //TODO créer un main permettant de tester la classe Gaulois 
 		
 		
